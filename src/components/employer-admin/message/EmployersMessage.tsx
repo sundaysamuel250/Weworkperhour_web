8d787803fc3adcb5e8975c1264c9a29e9dd05ba8
logiccamp <logiccmpl@gmail.com>
2024-08-27 11:19:31 +0100

    employer

--- src/components/employer-admin/message/EmployersMessage.tsx
+++ src/components/employer-admin/message/EmployersMessage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import EmployerChatBox from './components/EmployerChatBox'
+import ChatBox from '../../candidate-admin/message/components/ChatBox'
 
 const EmployersMessage: React.FC = () => {
   return (
     <div className="h-screen flex mt-[8rem]">
     <div className="w-full overflow-y-scroll">
-      <EmployerChatBox />
+      <ChatBox />
     </div>
   </div>
   )
